0d1737ac8e5e34361c8f801b66b864968b8378c5
Ilya Lokalin <ilyalokalin@gmail.com>
2024-06-13 18:12:00 +0300

    feat: add scrolling for channel changes

--- frontend/src/components/Chat/constants.js
+++ frontend/src/components/Chat/constants.js
@@ -1,4 +1,5 @@
 export const FIELD_MESSAGE = 'body';
+export const SCROLL_TYPE = 'smooth';
 
 export const initialValues = {
   [FIELD_MESSAGE]: '',
--- frontend/src/components/Chat/index.jsx
+++ frontend/src/components/Chat/index.jsx
@@ -1,18 +1,30 @@
+import { useEffect, useRef } from 'react';
 import { useSelector } from 'react-redux';
 import { selectCurrentChannel } from 'services/channelsApi';
 import { selectCurrentMessages } from 'services/messagesApi';
 import Message from './Message';
 import MessageForm from './MessageForm';
 import ChatHeader from './ChatHeader';
+import { SCROLL_TYPE } from './constants';
 
 const Chat = () => {
+  const pageRef = useRef(null);
   const channel = useSelector(selectCurrentChannel);
   const messages = useSelector(selectCurrentMessages);
+
+  useEffect(() => {
+    if (pageRef.current) {
+      pageRef.current.scrollTo({
+        top: pageRef.current.scrollHeight,
+        behavior: SCROLL_TYPE,
+      });
+    }
+  }, [channel, messages.length]);
 
   return (
     <div className="d-flex flex-column h-100">
       <ChatHeader channelName={channel?.name} count={messages.length} />
-      <div className="chat-messages overflow-auto px-5 ">
+      <div ref={pageRef} className="chat-messages overflow-auto px-5 ">
         {messages.map(({ id, username, body }) => (
           <Message key={id} username={username} body={body} />
         ))}
